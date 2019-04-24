11020107c80580fd4b142769f860748c403aece8
rm <sirycity2018@gmail.com>
2019-04-24 21:52:18 +0900

    mdに更に移行中

--- pages/privacy.vue
+++ pages/privacy.vue
@@ -4,30 +4,18 @@
       img(alt='プライバシーポリシー' src='~/assets/favicon.svg')
       h2 プライバシーポリシー
       p 個人情報の扱いについて
-    h3 予定表の情報について
-    p
-      | 四文字予定では、予定表の情報管理にローカルストレージを利用しています。
-    p
-      | ローカルストレージのデータはあなたのスマートフォン、タブレット、もしくはパソコンの中に保管されます。
-      | アプリ開発者への送信は一切行われませんので、ご安心ください。
-    p
-      | ローカルストレージに関して、詳しくは
-      a(href='https://www.w3schools.com/html/html5_webstorage.asp') こちら
-      | をご覧ください。
-    h3 アクセス解析ツールについて
-    p
-      | 四文字予定では、Googleによるアクセス解析ツール「Googleアナリティクス」を利用しています。
-      | このGoogleアナリティクスはトラフィックデータの収集のためにCookieを使用しています。
-    p
-      | このトラフィックデータは匿名で収集されており、個人を特定するものではありません。
-      | この機能はCookieを無効にすることで収集を拒否することができますので、お使いのブラウザの設定をご確認ください。
-    p
-      | この規約に関して、詳しくは
-      a(href='https://www.google.com/analytics/terms/jp.html') こちら
-      | 、または
-      a(href='https://policies.google.com/technologies/partner-sites?hl=ja') こちら
-      | をご覧ください。
+    div(v-html='privacy')
 </template>
+
+<script>
+import privacy from '~/assets/md/privacy.md'
+export default {
+  computed:{
+    privacy: ()=> privacy
+  }
+}
+</script>
+
 
 <style lang="stylus" scoped>
 .article
@@ -36,7 +24,7 @@
   min-height calc(var(--static100vh) - 80px)
   padding 40px 0
   text-align left
-  h3
+  >>> h3
     margin 45px 0 3px 3px
     border-left 7px solid teal
     padding-left 7px
