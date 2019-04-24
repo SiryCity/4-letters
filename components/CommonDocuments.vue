7957efc021619213f7938bed41224e6ec6b87ffa
rm <sirycity2018@gmail.com>
2019-04-24 22:19:41 +0900

    mdに半分移行

---
+++ components/CommonDocuments.vue
@@ -0,0 +1,57 @@
+<template lang="pug">
+  article.common-documents
+    div.common-documents__heading
+      img(:alt='title' :src='image')
+      h2 {{title}}
+      p {{subTitle}}
+    div(v-html='md' v-if='md')
+    slot
+</template>
+
+<script>
+export default {
+  props:['title', 'subTitle', 'image', 'md']
+}
+</script>
+
+
+<style lang="stylus" scoped>
+.common-documents
+  width 95%
+  max-width 540px
+  min-height calc(var(--static100vh) - 80px)
+  padding 40px 0
+  text-align left
+  display flex
+  flex-direction column
+  align-items center
+  >>> h3
+    margin 45px 0 3px 3px
+    border-left 7px solid teal
+    padding-left 7px
+  p
+    line-height 170%
+  
+  .common-documents__heading
+    width 100vw
+    height 120px
+    position relative
+    img
+      width 100%
+      height 120px
+      object-fit cover
+      background-color #999
+      opacity .5
+    h2
+      position absolute
+      top 8px
+      right 0
+      left 0
+      text-align center
+    p
+      position absolute
+      right 0
+      left 0
+      bottom 8px
+      text-align center
+</style>
--- pages/privacy.vue
+++ pages/privacy.vue
@@ -1,48 +1,23 @@
 <template lang="pug">
-  article.article
-    div.img-box
-      img(alt='プライバシーポリシー' src='~/assets/favicon.svg')
-      h2 プライバシーポリシー
-      p 個人情報の扱いについて
-    div(v-html='privacy')
+  common-documents(
+    title='プライバシーポリシー'
+    subTitle='個人情報の扱いについて'
+    :image='favicon'
+    :md='privacy'
+  )
 </template>
 
 <script>
+import CommonDocuments from '~/components/CommonDocuments'
 import privacy from '~/assets/md/privacy.md'
+import favicon from '~/assets/favicon.svg'
 export default {
+  components:{
+    CommonDocuments
+  },
   computed:{
-    privacy: ()=> privacy
+    privacy: () => privacy,
+    favicon: () => favicon
   }
 }
 </script>
-
-
-<style lang="stylus" scoped>
-.article
-  width 95%
-  max-width 540px
-  min-height calc(var(--static100vh) - 80px)
-  padding 40px 0
-  text-align left
-  >>> h3
-    margin 45px 0 3px 3px
-    border-left 7px solid teal
-    padding-left 7px
-  p
-    line-height 170%
-  
-  .img-box
-    width 100%
-    height 200px
-    display flex
-    flex-direction column
-    justify-content flex-start
-    align-items center
-    padding-top 40px
-    img
-      width 72px
-      height 72px
-    h2
-      margin 16px 0 2px
-      padding 0
-</style>
